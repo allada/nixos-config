2b1a74ed968443c6ed75c4fb08921abbc809e415
Your Name <you@example.com>
2025-07-22 03:29:40 +0000

    changes4

--- modules/desktop.nix
+++ modules/desktop.nix
@@ -1,8 +1,9 @@
 { config, pkgs, ... }: {
+  services.desktopManager.gnome.enable = true;
+
   # X11 and desktop environment
   services.xserver = {
     enable = true;
-    desktopManager.gnome.enable = true;
     
     # Keyboard configuration
     xkb = {
